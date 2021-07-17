bfde35590a11912063d6e2cecb4fc6d240f9687c
jonahcasey <jnhcasey@gmail.com>
2021-07-17 16:40:31 +1000

    made todo class.

---
+++ header.hpp
@@ -0,0 +1,16 @@
+
+
+class todo {
+
+   std::string name;
+   bool is_done;
+
+ public:
+
+       void set_name (std::string new_name)
+     Std::string get_name()
+    void set_state (bool state)
+  bool get_state()
+
+
+}
